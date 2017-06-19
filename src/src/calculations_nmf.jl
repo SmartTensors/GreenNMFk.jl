6e0d9bcb9c13845b4381e22d6a4cde8dc7bce7d9
Daniel Livingston <daniel.livingston@live.com>
2017-06-19 17:44:37 -0600

    latest nmf solver

--- src/src/calculations_nmf.jl
+++ src/src/calculations_nmf.jl
@@ -1,28 +1,39 @@
-# Calculation of the solution with j sources
+# Purpose: 	Calculation of the solution with j sources
+#			Minimizes the function: ∑ᵢ(MixFn[i]- ∑ⱼ(Sources[i,j]))² 
+# 
 # Input:
 #	number_of_sources - Number of sources
-#	nd 	 - 
-#	Nsim - 
-#	aa	 - 
-#	xD	 - 
-#	t0	 - 
-#	time - 
-#	S	 - 
-#	numT - 
+#	nd 	 - Number of detectors
+#	Nsim - Number of simulations
+#	aa	 - Multiple for initial random conditions
+#	xD	 - Detector positions
+#	t0	 - Initial time of sources
+#	time - Time vector from t_initial to t_final
+#	S	 - Observation matrix (returned from initial_conditions)
+#	numT - Number of time points
+
+# Returns:
+#	
 function calculations_nmf_v02(number_of_sources, nd, Nsim, aa, xD, t0, time, S, numT)
 	
 	GreenNMFk.log("\nRunning NMF calculation...")
 	GreenNMFk.log("-----------------------------------------")
 	
-	sol = zeros(Nsim, 3 * number_of_sources + 3)
-	normF = zeros(Nsim, 1)
+	sol = zeros(Nsim, 3 * number_of_sources + 3) # Solution matrix
+	normF = zeros(Nsim, 1) # 
 	normCut = 0
 	Qyes = 0
 	
 	# Define the function that will be minimized
 	# funF = ∑ᵢ(MixFn[i]- ∑ⱼ(Sources[i,j]))² 
-	
 	GreenNMFk.log("  Finding function to be minimized")
+	
+	# Create empty array for which each new function iteration will be appended
+	# The idea is to avoid an infinite recursion, i.e.:
+	# a = x->2x, b = x->x^2
+	# a = x->a(x)+b(x) != x->2x+x^2, instead it tries to call itself
+	fun_sum = Any[]
+	
 	for i=1:nd
 		if number_of_sources == 1
 			Mixfn = x -> source(time, x[4:6], xD[i,:], x[1:2], t0, x[3])
@@ -32,24 +43,26 @@ function calculations_nmf_v02(number_of_sources, nd, Nsim, aa, xD, t0, time, S, 
 					Mixfn   = x -> source(time, x[4:6], xD[i,:], x[1:2], t0, x[3])
 				else
 					mixfun2 = x -> source(time, x[d*3+1:d*3+3], xD[i,:], x[1:2], t0, x[3])
+					# This may have to be changed when testing n_sources > 1
+					# mimic the push! functionality
 					Mixfn   = let Mixfn = Mixfn; x -> Mixfn(x) + mixfun2(x); end
 				end
 			end
 		end
 		
-		# TODO: size(S[i,:]) = (720,)
-		#		size(zeros(1, (nd-1)*numT))) = (1,640)
-		# Is this accurate? Should Mixfn(x) make up the missing 80?
-		# [xx xxx] = [ xxxxx ]
-		# Verify this before moving to non-linear solver
 		if (i == 1)
-			funF = x -> ([Mixfn(x) zeros(1, (nd-1)*numT)] - S[i,:]')
+			push!(fun_sum, x -> ([Mixfn(x) zeros(1, (nd-1)*numT)] - S[i,:]))
+			#funF = x -> ([Mixfn(x) zeros(1, (nd-1)*numT)] - S[i,:]')
 		else
-			fun2 = x -> ([zeros(1, (i-1)*numT) Mixfn(x) zeros(1, (nd-i)*numT)] - S[i,:]')
-			funF = let funF = funF; x -> funF(x) + fun2(x); end
+			fun2 = x -> ([zeros(1, (i-1)*numT) Mixfn(x) zeros(1, (nd-i)*numT)] - S[i,:])
+			push!(fun_sum, x->fun_sum[i](x)+fun2(x))
+			#funF = let funF = funF; x -> funF(x) + fun2(x); end
 		end
 	end
 	
+	# Define funF as the final element in the function vector
+	funF = fun_sum[end]
+
 	GreenNMFk.log("  Calculating simulation parameters")
 	
 	# Defining the lower and upper boundary for the minimization
@@ -58,7 +71,7 @@ function calculations_nmf_v02(number_of_sources, nd, Nsim, aa, xD, t0, time, S, 
 	
 	# This loop is on the number of sources we investigate
 	# We need limits for all sources (ampl and coord)
-	for jj=1:number_of_sources
+	for jj = 1:number_of_sources
 		lb = [lb 0 -aa -aa] # General lower boundary [Ux Dx Dy A X Y]
 		ub = [ub 1.5 aa aa] # General upper boundary [Ux Dx Dy A X Y]
 	end
@@ -104,16 +117,12 @@ function calculations_nmf_v02(number_of_sources, nd, Nsim, aa, xD, t0, time, S, 
 		#parfor k = 1:Nsim
 		GreenNMFk.log("  -> Calculating the non-linear least squares fit")
 		
-		println(lb)
-		println(ub)
+		model_NMF = JuMP.Model(solver=Ipopt.IpoptSolver())
+		@JuMP.variable(model_NMF, lb[i] <= x[i=1:6] <= ub[i])
+		JuMP.register(model_NMF, :funF, 1, funF, autodiff=true)
 		
-		model_NMF = Model()#solver=sdfds(max_iters=3000))
-		@NLobjective(model_NMF, Min, funF)
-		@defVar(model_NMF, x[1:6])
-#		@defVar(model_NMF, lb[i] <= x[i=1:4] <= ub[i])
-#		@setConstraint(model_NMF, x < ub)
-#		@setConstraint(model_NMF, x > lb)
-		status = solve(model_NMF)
+		@JuMP.NLobjective(model_NMF, Min, funF(x[1]))
+		status = JuMP.solve(model_NMF)
 		println("x = ", getValue(x))
 		
 		GreenNMFk.log(status)
@@ -159,6 +168,6 @@ function calculations_nmf_v02(number_of_sources, nd, Nsim, aa, xD, t0, time, S, 
 		JLD.save(joinpath(working_dir, outfile), "sol", sol, "normF", normF, "S", S, "lb", lb, "ub", ub, "AA", AA, "sol_real", sol_real, "normF_real", normF_real, "normF_abs", normF_abs, "DidItGoBack", DidItGoBack, "Qyes", Qyes)
 	end
 	
-	return [sol,normF,lb,ub, AA,sol_real, normF_real, normF1, sol_all, normF_abs, Qyes]
+	return sol, normF, lb, ub, AA, sol_real, normF_real, normF1, sol_all, normF_abs, Qyes
 end
 		
